a6095ae73e3dd1a1a5cf567eeaa4d56e0194bf12
sumit26696 <sumit26696@gmail.com>
2021-07-12 00:25:39 +0530

    Revert "React-App update"
    This reverts commit a44b19c5087c8b89337f00f99c113da32ba3875f.

--- src/app/components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
-export { default as TokenProgress } from "./TokenProgress";
-export { default as Intro } from "./Intro";
-export { default as CoopaPhases } from "./CoopaPhases";
