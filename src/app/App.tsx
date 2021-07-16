5f5163f673975a4b139da2f3297722e0478f3a89
Rahul Yadav <rahulkyadav57@gmail.com>
2021-07-17 02:10:54 +0530

    install ant design module

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
-import "./styles/app.scss";
 import { Footer, Header } from "./components/common";
 import AppRouter from "./AppRouter";
 import { FC } from "react";
+import "./styles/app.scss";
+import "antd/dist/antd.css";
 
 const App: FC = () => {
   return (
